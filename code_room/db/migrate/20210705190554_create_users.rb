ce3c53702c106b39be0f1501f242157d93603b7e
Trevor Almon <trevoralmon@gmail.com>
2021-07-06 21:38:22 -0400

    initial migrations

---
+++ code_room/db/migrate/20210705190554_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users, id: :uuid do |t|
+      t.string :username, null: false
+      t.string :password_digest, null: false
+      t.string :email, null: false
+      t.integer :friends, array: true, default: []
+      t.index :email, unique: true
+      t.timestamps
+    end
+  end
+end
