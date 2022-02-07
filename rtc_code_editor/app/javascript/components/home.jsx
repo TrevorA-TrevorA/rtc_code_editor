6b6e4a5dec2a1959364bae9363163b03831bb45a
Trevor Almon <trevoralmon@gmail.com>
2022-02-06 19:39:36 -0500

    Add intro page

--- rtc_code_editor/app/javascript/components/home.jsx
+++ rtc_code_editor/app/javascript/components/home.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import LoginContainer from '../containers/login_container';
+// import LoginContainer from '../containers/login_container';
 import { Redirect } from 'react-router-dom'
+import IntroDisplay from './intro_display';
 
 class Home extends React.Component {
   constructor(props) {
     super(props)
   }
   render() {
-    return this.props.user ? <Redirect to="/dash"/> : <LoginContainer/>;
+    return this.props.user ? <Redirect to="/dash"/> : <IntroDisplay/>;
   }
 }
 
