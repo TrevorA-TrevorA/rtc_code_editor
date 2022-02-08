52ab740ea7937e3aa22326c18746bf0024fe82dd
Trevor Almon <trevoralmon@gmail.com>
2022-02-07 19:42:36 -0500

    Remove unnecessary dependency

--- rtc_code_editor/app/javascript/store.js
+++ rtc_code_editor/app/javascript/store.js
@@ -1,6 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
 import rootReducer from './reducers/root_reducer';
 const loggedIn = !!window.currentUser;
 const documents = loggedIn ? window.currentUser.documents : [];
@@ -16,4 +15,4 @@ const initialState = {
   selected: []
 }
 
-export const store = createStore(rootReducer, initialState, applyMiddleware(logger, thunk));
+export const store = createStore(rootReducer, initialState, applyMiddleware(thunk));
