76ac55423753cd2d137560a5deb229223d4b47eb
Trevor Almon <trevoralmon@gmail.com>
2022-03-05 22:33:04 -0500

    Fix video coordination issue

--- rtc_code_editor/app/javascript/components/intro_display.jsx
+++ rtc_code_editor/app/javascript/components/intro_display.jsx
@@ -5,16 +5,18 @@ import dataDemo from 'videos/data_view.mp4';
 
 const IntroDisplay = () => {
 
+  const syncPlay = () => $(".demo-vid").each((_, v) => v.currentTime = 0);
+
   return (<div className='intro-view'>
     <div className='video-block'>
-      <video className="demo-vid" src={worfDemo} autoPlay loop muted/>
+      <video className="demo-vid" onPlay={syncPlay} src={worfDemo} autoPlay loop muted/>
       <div className='auth-link'>
         <p>Sign In</p>
         <Link to="/log-in"/>
       </div>
     </div>
     <div className='video-block'>
-      <video className="demo-vid" src={dataDemo} autoPlay loop muted/>
+      <video className="demo-vid" onPlay={syncPlay} src={dataDemo} autoPlay loop muted/>
       <div className='auth-link'>
         <p>Create Account</p>
         <Link to="/sign-up"/>
