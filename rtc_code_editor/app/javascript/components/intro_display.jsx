b62e2f3751166b1ad9ab4c2d6cbc1c3d0325979e
Trevor Almon <trevoralmon@gmail.com>
2022-03-05 21:29:21 -0500

    Fix video coordination issue

--- rtc_code_editor/app/javascript/components/intro_display.jsx
+++ rtc_code_editor/app/javascript/components/intro_display.jsx
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import worfDemo from 'videos/worf_view.mp4';
 import dataDemo from 'videos/data_view.mp4';
 
 const IntroDisplay = () => {
 
+  useEffect(() => {
+    const coord = setInterval(() => {
+      if (document.readyState !== "complete") return;
+      clearInterval(coord);
+      $(".demo-vid").trigger('play');
+    },100)
+  })
+
   return (<div className='intro-view'>
     <div className='video-block'>
-      <video className="demo-vid" src={worfDemo} autoPlay loop muted/>
+      <video className="demo-vid" src={worfDemo} loop muted/>
       <div className='auth-link'>
         <p>Sign In</p>
         <Link to="/log-in"/>
       </div>
     </div>
     <div className='video-block'>
-      <video className="demo-vid" src={dataDemo} autoPlay loop muted/>
+      <video className="demo-vid" src={dataDemo} loop muted/>
       <div className='auth-link'>
         <p>Create Account</p>
         <Link to="/sign-up"/>
