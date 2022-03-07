634e2b942ad10748278227881564c5e164bda4b2
Trevor Almon <trevoralmon@gmail.com>
2022-03-06 20:42:57 -0500

    Fix migration bug

--- rtc_code_editor/db/migrate/20220306185535_remove_old_col_rename_new_col.rb
+++ rtc_code_editor/db/migrate/20220306185535_remove_old_col_rename_new_col.rb
@@ -1,11 +1,4 @@
-require 'zlib'
-
 class RemoveOldColRenameNewCol < ActiveRecord::Migration[6.1]
-  Document.all.each do |doc|
-    compressed = Zlib::Deflate.deflate(doc.content)
-    doc.update(compress_content: compressed)
-  end
-
   def change
     remove_column :documents, :content
     rename_column :documents, :compress_content, :content
