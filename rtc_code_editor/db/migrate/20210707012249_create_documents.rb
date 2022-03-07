fe21e9e536c23bbab5495f1f356f716bff601c5b
Trevor Almon <trevoralmon@gmail.com>
2022-03-06 21:34:22 -0500

    Update migrations

--- rtc_code_editor/db/migrate/20210707012249_create_documents.rb
+++ rtc_code_editor/db/migrate/20210707012249_create_documents.rb
@@ -2,7 +2,7 @@ class CreateDocuments < ActiveRecord::Migration[6.1]
   def change
     create_table :documents, id: :uuid do |t|
       t.string :file_name, null: false
-      t.text :content
+      t.binary :content
       t.references :room, foreign_key: true, type: :uuid
       t.references :admin, foreign_key: { to_table: :users }, type: :uuid
       t.timestamps
--- rtc_code_editor/db/migrate/20220306185535_remove_old_col_rename_new_col.rb
+++
@@ -1,6 +0,0 @@
-class RemoveOldColRenameNewCol < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :documents, :content
-    rename_column :documents, :compress_content, :content
-  end
-end
